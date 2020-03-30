fcc28e8378dfab9ce1a23aabee1625a2c8df21fb
sill2673 <sill2673@stud.kea.dk>
2020-03-30 13:56:30 +0200

    .

---
+++ script.js
@@ -0,0 +1,27 @@
+ScrollReveal().reveal('#omMig', {
+    delay: 900
+});
+
+
+ScrollReveal().reveal('.opgave1', {
+    delay: 900
+});
+ScrollReveal().reveal('.opgave2', {
+    delay: 900
+});
+ScrollReveal().reveal('.opgave3', {
+    delay: 900
+});
+ScrollReveal().reveal('.opgave4', {
+    delay: 900
+});
+ScrollReveal().reveal('.opgave5', {
+    delay: 900
+});
+
+
+// When the user clicks on <div>, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
