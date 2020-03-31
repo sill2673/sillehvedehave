5bb025aecd48b66a8a2126e90a6fbe1b64636069
sill2673 <sill2673@stud.kea.dk>
2020-03-31 14:13:49 +0200

    meget

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-ScrollReveal().reveal('#omMig', {
+ScrollReveal().reveal('.top_overskrift', {
     delay: 900
 });
 
@@ -18,6 +18,15 @@ ScrollReveal().reveal('.opgave4', {
 ScrollReveal().reveal('.opgave5', {
     delay: 900
 });
+ScrollReveal().reveal('.opgave6', {
+    delay: 900
+});
+ScrollReveal().reveal('.opgave7', {
+    delay: 900
+});
+ScrollReveal().reveal('.opgave8', {
+    delay: 900
+});
 
 
 ScrollReveal().reveal('#kompetencer', {
@@ -26,10 +35,36 @@ ScrollReveal().reveal('#kompetencer', {
 ScrollReveal().reveal('#cv', {
     delay: 900
 });
+ScrollReveal().reveal('.popup', {
+    delay: 900
+});
+ScrollReveal().reveal('.blomster_under_erfaring', {
+    delay: 900
+});
+
+ScrollReveal().reveal('#', {
+    delay: 900
+});
+ScrollReveal().reveal('.', {
+    delay: 900
+});
+ScrollReveal().reveal('.', {
+    delay: 900
+});
+ScrollReveal().reveal('.', {
+    delay: 900
+});
+
+
 
 
 // When the user clicks on <div>, open the popup
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
+}
+
+function myFunction() {
+    var popup2 = document.getElementById("myPopup2");
+    popup2.classList.toggle("show");
 }
